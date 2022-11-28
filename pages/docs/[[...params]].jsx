3dd30494e95496282595b21a69607b58c8c4fbd3
SakibAlIslam <sakib35-1680@diu.edu.bd>
2022-11-29 03:12:44 +0600

    update some text

--- pages/docs/[[...params]].jsx
+++ pages/docs/[[...params]].jsx
@@ -6,9 +6,9 @@ const Doc = () => {
     const { params = []} = router.query;
     console.log(params)
     if (params.length === 2) {
-        return (<h1>Viewing docs for feature {params[0]} and concept {params[1]}</h1>)
+        return (<h1>Viewing docs for {params[0]} and {params[1]}</h1>)
     } else if (params.length === 1) {
-        return (<h1>Viewing docs for feature {params[0]}</h1>)
+        return (<h1>Viewing docs for {params[0]}</h1>)
     }
     return <div>Doc Home Page - </div>
 }
